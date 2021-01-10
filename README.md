93c5a89b4ba97e72341d407095cd0599902d3b0a
mit1013 <74683455+mit1013@users.noreply.github.com>
2021-01-10 20:53:17 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# mt-useState
+# react-test
 Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [{ count1, count2 }, setCount] = useState({ count1: 10, count2: 20 });
+  console.log("count1", count1);
+  console.log("count2", count2);
+  return (
+    <div className="App">
+      <div>count1: {count1}</div>
+      <div>count2: {count2}</div>
+      <button
+        onClick={() =>
+          setCount((currentState) => ({
+            ...currentState,
+            count1: currentState.count1 + 1
+          }))
+        }
+      >
+        increment
+      </button>
+    </div>
+  );
+}
